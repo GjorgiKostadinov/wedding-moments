2de8cc5e62578dec74b62c574cf6b0f594ee3669
Костадинов Ѓорги <gjorgi.kostadinov@students.finki.ukim.mk>
2025-07-28 13:00:32 +0200

    Version 8.0.0 (Email)

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Правилна конфигурација за Next.js 15
   serverExternalPackages: ['nodemailer'],
-  
   webpack: (config) => {
     config.externals = [...(config.externals || []), 'nodemailer'];
     return config;
